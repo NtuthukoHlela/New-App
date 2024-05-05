8c571ba04182e07b1db4688648a8e6fa848cbb91
Ntuthuko Hlela <111696810+NtuthukoHlela@users.noreply.github.com>
2024-05-05 02:49:01 +0200

    Initial Commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+console.log("Hello World, this is super amazing!")
+
+//creating a react app: npm init react-app {app name}
+
+// When using a Youtube Link, do this: "https://www.youtube.com/embed/1WpqQfmzBGY" instead of this: https://www.youtube.com/watch?v=1WpqQfmzBGY
+
+//Installing the doc viewer 
+//npm i @cyntler/react-doc-viewer    **i stands for install 
